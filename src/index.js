6c25e0d3b2c8f92137fe609ddd3961824244ae65
Manan Taneja <manantaneja007@gmail.com>
2022-01-27 07:36:21 +0530

    feat: used ThemeProvider to change themes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDom from 'react-dom'
+import { PrimaryButton, SecondaryButton, TertiaryButton } from './components'
+import { GlobalStyle } from './utils'
+import { ThemeProvider } from 'styled-components'
+import { darkTheme, defaultTheme } from './utils'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const [useDarkTheme, setUseDarkTheme] = useState(false)
+    return (
+        <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+            <button
+                style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+                onClick={() => setUseDarkTheme(true)}
+            >
+                Dark theme
+            </button>
+            <button
+                style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+                onClick={() => setUseDarkTheme(false)}
+            >
+                Default theme
+            </button>
+            <div
+                style={{
+                    background: useDarkTheme
+                        ? defaultTheme.primaryColor
+                        : darkTheme.primaryColor,
+                    width: "100vw",
+                    height: "90vh",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-around"
+                }}
+            >
+                <PrimaryButton modifiers={["small", "warning"]}>Hello World</PrimaryButton>
+                <SecondaryButton modifiers={["large"]}>Goodbye World</SecondaryButton>
+                <TertiaryButton>Hey</TertiaryButton>
+                <GlobalStyle />
+            </div>
+        </ThemeProvider>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDom.render(<App />, document.getElementById('root'))
